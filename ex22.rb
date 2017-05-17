122cbaa980a923444f8f2642774a4f275de38ccb
Vasya <vtomyev@gmai.com>
2017-05-16 23:08:40 -0700

    Ex 22 initial commit

---
+++ ex22.rb
@@ -0,0 +1,16 @@
+# - is called hash and is used to comment or can be used in #{} to evaluate an expression in a string
++ used to add
+= used to assign variables
+- subtract
+* multiply
+/ divide
+% modulus
+return - returns output from a function
+def - define function
+end - ends function
+puts - prints out a string on a new line
+print - prinst out a string on the same line
+
+\ - escape character ex. "I want \"this to print out within this string\" but also be in double quotes"
+"" - string literal
+'' - same as above
