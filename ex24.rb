2a4d2129f6b8aa108095f1414eab3b7590eef955
Vasya <vtomyev@gmail.com>
2017-06-13 21:26:50 -0700

    Ex25 initial commit

--- ex24.rb
+++ ex24.rb
@@ -17,15 +17,17 @@ END
 
  five = 10 - 2 + 3 - 6
  puts "This should be a five: #{five}"
-
+#define a function secret_formula with an argument
  def secret_formula(started)
    jelly_beans = started * 500
    jars = jelly_beans / 1000
    crates = jars / 100
+   #return's the values of above variables
    return jelly_beans, jars, crates
  end
-
+#start point variable set
  start_point = 10000
+ #three vars are set to be the output of secret_formula function with 10000 as the starting point.
  beans, jars, crates = secret_formula(start_point)
 
  puts "With a starting point of: #{start_point}"
@@ -33,4 +35,5 @@ END
 
  start_point = start_point / 10
  puts "We can also do that this way:"
+ #The below line does not work right for some reason... maybe the new ruby does not accept % vars in strings.
  puts "We'd have %s beans, %d jars, and %d crates"
